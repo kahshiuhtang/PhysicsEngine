273665e8463dabb2b2f8ba5898794533822d4241
Kah Shiuh Tang <kahshiuh.tang@gmail.com>
2024-05-30 22:37:04 -0400

    gravity + basic bounce upwards, needs to be refactored

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,60 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
+#include <unistd.h>
+#include <iostream>
+#include <cstdlib>
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
-
+    sf::RenderWindow window(sf::VideoMode(1024, 512), "Physics Engine");
+    sf::CircleShape circle(20.f);
+    circle.setFillColor(sf::Color::Green);
+    circle.setFillColor(sf::Color::Green);
+    circle.setPosition(375.0f, 275.0f);  
+    float currentVel = 0.0f;
+    float currentPos = 275.0f;
+    float const gravity = 9.8f;
+    float time = 0.01;
+    bool hitGround = false;
     while (window.isOpen())
     {
         sf::Event event;
         while (window.pollEvent(event))
         {
-            if (event.type == sf::Event::Closed)
+            if (event.type == sf::Event::Closed){
                 window.close();
+            }else if(event.type == sf::Event::KeyPressed){
+                if(event.key.code == sf::Keyboard::Left){
+                    circle.move(-2, 0);
+                }
+                else if(event.key.code == sf::Keyboard::Right){
+                    circle.move(2, 0);
+                }
+                else if(event.key.code == sf::Keyboard::Up){
+                    circle.move(0, -2);
+                }
+                else if(event.key.code == sf::Keyboard::Down){
+                    circle.move(0, 2);
+                }
+            }
         }
-
         window.clear();
-        window.draw(shape);
+        circle.setPosition(375.0f, currentPos); 
+        window.draw(circle);
         window.display();
+        std::cout << currentPos << "\n";
+        if(hitGround || currentPos > 500){
+            hitGround = true;
+            currentPos -= currentVel;
+        }else{
+            currentPos = currentPos - (0.5 * - gravity * time * time) / 10;
+            currentVel = (gravity * time ) / 10;
+        }
+        if(currentPos < 10){
+            hitGround = false;
+        }
+        time += 0.01;
+        usleep(10000);
     }
 
     return 0;
